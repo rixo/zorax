ad3663b14f49afdb723432e8b90c321412d9338e
rixo <rixo@rixo.fr>
2020-01-21 00:56:57 +0100

    alias

---
+++ packages/zorax/test/alias.spec.js
@@ -0,0 +1,90 @@
+import { describe, test } from 'zorax'
+
+import { createHarness } from '@/lib/plug'
+import withAlias from '@/lib/alias'
+
+import { isHarness } from './util'
+
+describe(__filename)
+
+test('does not break createHarness', t => {
+  const z = createHarness([withAlias({})])
+  t.ok(isHarness(z))
+})
+
+test('withAlias({ from: to | [...tos] })', t => {
+  const foo = {}
+  const bar = {}
+  const baz = {}
+  const plugin = {
+    test: z => {
+      z.foo = foo
+      z.baz = baz
+    },
+    harness: h => {
+      h.bar = bar
+    },
+  }
+
+  const z = createHarness([
+    withAlias({
+      test: {
+        foo: ['foot', 'foob'],
+        baz: 'zap',
+        nonExistant: 'pif',
+      },
+      harness: {
+        foo: 'fooh',
+        bar: ['barb', 'babar'],
+      },
+    }),
+    plugin,
+  ])
+
+  t.ok(isHarness(z))
+
+  // sanity
+  t.is(z.foo, foo)
+  t.is(z.bar, bar)
+
+  {
+    let hasRun = false
+    z.test('', z => {
+      t.is(z.foo, foo)
+      t.is(z.bar, undefined)
+      hasRun = true
+    })
+    t.ok(hasRun)
+  }
+
+  t.test('aliases are copied to the harness', t => {
+    t.test('test context aliases', t => {
+      t.is(z.foot, foo, 'test alias multi (1)')
+      t.is(z.foob, foo, 'test alias multi (2)')
+      t.is(z.zap, baz, 'test alias single')
+      t.is(z.pif, undefined, 'non existant target')
+    })
+    t.test('harness specific aliases', t => {
+      t.is(z.fooh, foo, 'harness specific single')
+      t.is(z.barb, bar, 'harness specific multiple')
+      t.is(z.babar, bar, 'harness specific multiple')
+      t.is(z.pif, undefined, 'non existant target')
+    })
+  })
+
+  t.test('aliases are copied to all test context', t => {
+    let hasRun = false
+    z.test('main', z => {
+      z.test('sub', z => {
+        z.test('sub sub', () => {
+          t.is(z.foot, foo, 'test alias multi (1)')
+          t.is(z.foob, foo, 'test alias multi (2)')
+          t.is(z.zap, baz, 'test alias single')
+          t.is(z.pif, undefined, 'non existant target')
+          hasRun = true
+        })
+      })
+    })
+    t.ok(hasRun)
+  })
+})
