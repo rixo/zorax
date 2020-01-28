470c3309f00e2219c0050cc5016cc1549707211d
rixo <rixo@rixo.fr>
2020-01-28 23:10:24 +0100

    clean

--- packages/zorax/lib/zorax.js
+++ packages/zorax/lib/zorax.js
@@ -1,40 +1,7 @@
-import createNextHarness from './zorax.defaults'
+import createDefaultHarness from './zorax.defaults'
 
 export { createHarness, createHarnessFactory } from './plug'
 
-// export const harness = createDefaultHarness()
-export const harness = createNextHarness()
+export const harness = createDefaultHarness()
 
 export const { test, group, describe, plug } = harness
-
-// setTimeout(() => {
-//   harness.report(reporter())
-//   // harness.report()
-// })
-
-// export { AssertPrototype, mochaTapLike, tapeTapLike } from 'zora'
-//
-// export const {
-//   deepEqual,
-//   doesNotThrow,
-//   eq,
-//   equal,
-//   equals,
-//   fail,
-//   falsy,
-//   is,
-//   isNot,
-//   notDeepEqual,
-//   notEq,
-//   notEqual,
-//   notEquals,
-//   notOk,
-//   notSame,
-//   ok,
-//   only,
-//   same,
-//   skip,
-//   test,
-//   throws,
-//   truthy,
-// } = harness
