b19f3a40e056b9efa6dc0b80201093091564d59f
rixo <rixo@rixo.fr>
2020-02-11 02:06:58 +0100

    fix test watch

--- .zoarrc.js
+++ .zoarrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-  // pattern(s) to find test files
   files: ['test/**/*.spec.js', '!test/find/sample'],
 
-  // patterns of files to watch
   watch: ['**/*.{js,css,html,json}'],
 
   watchDebounce: 10,
 
-  // we're watching dist for tests, so we ignore lib to avoid an early event and
-  // possible bouncing of tests (running multiple times)
-  ignore: ['**/node_modules', '**/.git', 'lib'],
+  ignore: ['**/node_modules', '**/.git', 'dist'],
 }
