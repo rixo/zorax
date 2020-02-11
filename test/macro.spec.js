04874c0fa3182f92f2171cfe42b6ef3d9dddadc3
rixo <rixo@rixo.fr>
2020-02-11 17:26:26 +0100

    fix skip & only interop

--- test/macro.spec.js
+++ test/macro.spec.js
@@ -3,9 +3,7 @@ import { test, describe } from '@@'
 import { blackHole, isFunction, isHarness, isTestContext, spy } from '@@/util'
 
 import { createHarness, createHarnessFactory } from '@/plug'
-import withDefer from '@/defer'
 import withMacro from '@/macro'
-import withOnly from '@/defer.only'
 
 describe(__filename)
 
@@ -13,41 +11,29 @@ const createHarnessWithMacro = createHarnessFactory([withMacro()])
 
 const createTestSpy = ({ hook = 'harness' } = {}) => {
   let zTest
+  let zSkip
+  let zOnly
   const test = spy((...args) => zTest(...args))
+  const skip = spy((...args) => zSkip(...args))
+  const only = spy((...args) => zOnly(...args))
   return {
     plugin: {
       // NOTE we only want the first wrapped one, for harness... the sub test
       // is never called in this test
       [hook]: spy(z => {
         zTest = z.test
+        zSkip = z.skip
+        zOnly = z.only
         z.test = test
+        z.skip = skip
+        z.only = only
       }),
     },
     test,
+    skip,
+    only,
   }
 }
-
-describe('dependencies', () => {
-  describe('must be before zorax.defer.only', () => {
-    test('throws if zorax.defer.only is before', t => {
-      t.throws(() => {
-        createHarness([withDefer(), withOnly(), withMacro()])
-      }, /zorax\.defer\.only/)
-    })
-
-    test('does not throw if zorax.defer.only is after', t => {
-      t.doesNotThrow(() => {
-        createHarness([withDefer(), withMacro(), withOnly()])
-      })
-    })
-
-    test('does not throw if zorax.defer.only is absent', t => {
-      t.doesNotThrow(() => {
-        createHarness([withDefer(), withMacro()])
-      })
-    })
-  })
-})
 
 test('exports a function', t => {
   t.ok(typeof withMacro === 'function', 'withMacro is a function')
@@ -101,6 +87,7 @@ describe('macro.title', () => {
       `${providedTitle}: ${a.name}-${b.name}`
 
     await z.test(desc, macro, a, b)
+
     t.eq(testSpy.plugin.harness.callCount, 1)
     // await z.report(blackHole)
     t.eq(macro.callCount, 1, 'macro was called')
@@ -181,6 +168,145 @@ describe('macro.title', () => {
     t.ok(
       isFunction(testSpy.test.calls[0][1]),
       'previous test is called with spec function'
+    )
+  })
+
+  test('t.skip(desc, macro, ...data)', async t => {
+    const desc = 'desc'
+    const a = { name: 'a' }
+    const b = { name: 'b' }
+    const testSpy = createTestSpy()
+    const z = createHarness([testSpy.plugin, withMacro()])
+    t.eq(testSpy.plugin.harness.callCount, 1)
+
+    const macro = spy((z, ...args) => {
+      t.test('macro has been called with context and data args', t => {
+        t.eq(args.length, 2)
+        t.is(args[0], a)
+        t.is(args[1], b)
+      })
+    })
+
+    macro.title = (providedTitle = 'dft', a, b) =>
+      `${providedTitle}: ${a.name}-${b.name}`
+
+    await z.skip(desc, macro, a, b)
+
+    t.eq(testSpy.plugin.harness.callCount, 1)
+    // await z.report(blackHole)
+    t.eq(macro.callCount, 0, 'macro was not called')
+    t.eq(testSpy.test.callCount, 0, 'core test not was called')
+    t.eq(testSpy.skip.callCount, 1, 'core skip was called')
+    t.eq(
+      testSpy.skip.calls[0][0],
+      'desc: a-b',
+      'previous skip is called with description'
+    )
+    t.ok(
+      isFunction(testSpy.skip.calls[0][1]),
+      'previous skip is called with spec function'
+    )
+  })
+
+  test('t.skip(macro, ...data)', t => {
+    const a = { name: 'a' }
+    const b = { name: 'b' }
+    const testSpy = createTestSpy()
+    const z = createHarness([testSpy.plugin, withMacro()])
+    t.eq(testSpy.plugin.harness.callCount, 1)
+
+    const macro = spy((z, ...args) => {
+      t.eq(args.length, 2)
+      t.is(args[0], a)
+      t.is(args[1], b)
+    })
+
+    macro.title = (providedTitle = 'dft', a, b) =>
+      `${providedTitle}: ${a.name}-${b.name}`
+
+    z.skip(macro, a, b)
+
+    t.eq(testSpy.plugin.harness.callCount, 1)
+    t.eq(macro.callCount, 0, 'macro was not called')
+    t.eq(testSpy.test.callCount, 0, 'core test was not called')
+    t.eq(testSpy.skip.callCount, 1, 'core skip was called')
+    t.eq(
+      testSpy.skip.calls[0][0],
+      'dft: a-b',
+      'previous skip is called with description'
+    )
+    t.ok(
+      isFunction(testSpy.skip.calls[0][1]),
+      'previous skip is called with spec function'
+    )
+  })
+
+  test('t.only(desc, macro, ...data)', async t => {
+    const desc = 'desc'
+    const a = { name: 'a' }
+    const b = { name: 'b' }
+    const testSpy = createTestSpy()
+    const z = createHarness([testSpy.plugin, withMacro()])
+    t.eq(testSpy.plugin.harness.callCount, 1)
+
+    const macro = spy((z, ...args) => {
+      t.test('macro has been called with context and data args', t => {
+        t.eq(args.length, 2)
+        t.is(args[0], a)
+        t.is(args[1], b)
+      })
+    })
+
+    macro.title = (providedTitle = 'dft', a, b) =>
+      `${providedTitle}: ${a.name}-${b.name}`
+
+    z.only(desc, macro, a, b)
+
+    t.eq(testSpy.plugin.harness.callCount, 1)
+    t.eq(macro.callCount, 0, 'macro was not called')
+    t.eq(testSpy.test.callCount, 0, 'core test not was called')
+    t.eq(testSpy.only.callCount, 1, 'core only was called')
+    t.eq(
+      testSpy.only.calls[0][0],
+      'desc: a-b',
+      'previous only is called with description'
+    )
+    t.ok(
+      isFunction(testSpy.only.calls[0][1]),
+      'previous only is called with spec function'
+    )
+  })
+
+  test('t.only(macro, ...data)', t => {
+    const a = { name: 'a' }
+    const b = { name: 'b' }
+    const testSpy = createTestSpy()
+    const z = createHarness([testSpy.plugin, withMacro()])
+    t.eq(testSpy.plugin.harness.callCount, 1)
+
+    const macro = spy((z, ...args) => {
+      t.eq(args.length, 2)
+      t.is(args[0], a)
+      t.is(args[1], b)
+    })
+
+    macro.title = (providedTitle = 'dft', a, b) =>
+      `${providedTitle}: ${a.name}-${b.name}`
+
+    z.only(macro, a, b)
+
+    t.eq(testSpy.plugin.harness.callCount, 1)
+    t.eq(macro.callCount, 0, 'macro was not called')
+    t.eq(testSpy.test.callCount, 0, 'core test was not called')
+    t.eq(testSpy.only.callCount, 1, 'core only was called')
+    t.eq(
+      testSpy.only.calls[0][0],
+      'dft: a-b',
+      'previous only is called with description'
+    )
+    t.ok(
+      isFunction(testSpy.only.calls[0][1]),
+      'previous only is called with spec function'
     )
   })
 
