85c2a243a1f326d9a01dc74ecb87336f52cf0d36
rixo <rixo@rixo.fr>
2020-02-15 22:05:23 +0100

    clean

--- test/zora.spec.js
+++ test/zora.spec.js
@@ -1,9 +1,7 @@
-import { test, describe } from '@@'
+import { test } from '@@'
 import * as zora from 'zora'
 
 import { spy, blackHole } from '@@/util'
-
-describe(__filename)
 
 test('extra test arguments are not passed down to test handler', async t => {
   const z = zora.createHarness()
